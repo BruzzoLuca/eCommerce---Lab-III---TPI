04a0fcfe601d8aa2a0e0de0adeab2ae485dd1231
BRUZZO.LUCA <luca.bruzzo99@gmail.com>
2024-11-03 21:53:06 -0300

    Creating project structure

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import './App.css'
+
+const SMARTPHONES = [
+  {
+    name: "iPhone 13 Pro",
+    brand: "Apple",
+    imageUrl: "",
+  },
+  {
+    name: "Samsung Galaxy S24 Ultra",
+    brand: "Samsung",
+    imageUrl: "",
+  },
+  {
+    name: "Google Pixel 6 Pro",
+    brand: "Google",
+    imageUrl: "",
+  },
+  {
+    name: "Motorola Edge 20 Pro",
+    brand: "Motorola",
+    imageUrl: " ",
+  },
+];
+
+function App() {
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
+
